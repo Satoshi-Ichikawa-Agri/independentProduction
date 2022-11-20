4a3e3e316985e0a0a53d421e3e63e7e5733b87c5
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-21 07:02:19 +0900

    create image_recognition.py

--- image_Recognition.py
+++ image_Recognition.py
@@ -1,6 +1,28 @@
-"""個体の識別をする
+"""特定物体の識別処理
+
 """
-import cv2
+import sys
+import cv2 as cv
+
+def detect(imagefilename, cascadefilename):
+    srcimg = cv.imread(imagefilename)
+    if srcimg is None:
+        print('cannot load image')
+        sys.exit(-1)
+    dstimg = srcimg.copy()
+    cascade = cv.CascadeClassifier(cascadefilename)
+    if cascade.empty():
+        print('cannnot load cascade file')
+        sys.exit(-1)
+    objects = cascade.detectMultiScale(srcimg, 1.1, 3)
+    for (x, y, w, h) in objects:
+        print(x, y, w, h)
+        cv.rectangle(dstimg, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    return dstimg
+
+if __name__ == '__main__':
+    result = detect('images/cat.jpg', 'cascade.xml')
+    cv.imwrite('images/result.jpg', result)
 
 
 
